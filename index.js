e30a70206c9b71272119c4c84104a6c0d101b48b
anasshah2000 <shahanas4069@gmail.com>
2024-12-31 10:51:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+
+
+const circle = document.getElementById('circle');
+
+
+setTimeout(function() {
+    
+    circle.style.backgroundColor = 'yellow ';
+}, 2000); 
+
+const light = document.getElementById('light');
+
+
+setTimeout(function light() {
+    
+    light.style.backgroundColor = 'pink ';
+}, ) 
+
+
+        
+        
